a4dc3dce4878c36fde77c0764a456466f4090284
Murad <murad186992@mail.ru>
2022-08-10 16:42:10 +0300

    catPizza

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -3,30 +3,32 @@ import Categories from "./components/Categories";
 import Header from "./components/Header";
 import PizzaBlock from "./components/PizzaBlock";
 import Sort from "./components/Sort";
-
 import "./scss/app.scss";
+import pizzas from "./assets/pizza.json";
 
 function App() {
   return (
     <div className="wrapper">
-      
       <Header />
 
       <div className="content">
         <div className="container">
           <div className="content__top">
-
             <Categories />
 
             <Sort />
-
           </div>
           <h2 className="content__title">Все пиццы</h2>
           <div className="content__items">
-
-            <PizzaBlock title="Мексиканская" price={500}/>
-            <PizzaBlock title="Московская" price={350}/>
-           
+            {pizzas.map((el, i) => (
+              <PizzaBlock
+                title={el.title}
+                price={el.price}
+                image={el.imageUrl}
+                sizes={el.sizes}
+                types={el.types}
+              />
+            ))}
           </div>
         </div>
       </div>
