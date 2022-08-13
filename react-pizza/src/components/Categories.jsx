ef0303de8cde0df7beaa352cbc339b081d9c8d9f
Murad <murad186992@mail.ru>
2022-08-13 16:21:44 +0300

    sort

--- react-pizza/src/components/Categories.jsx
+++ react-pizza/src/components/Categories.jsx
@@ -1,22 +1,30 @@
-import { useState } from "react"
+import React from 'react'
 
-function Categories() {
-  const [activeIndex, setActiveIndex] = useState(0)
+function Categories({ value, onChangeCategory }) {
+  const categories = [
+    "Все",
+    "Мясные",
+    "Вегетарианская",
+    "Гриль",
+    "Острые",
+    "Закрытые",
+  ];
 
-  const categories = ['Все', 'Мясные', 'Вегетарианская', 'Гриль', 'Острые', 'Закрытые']
-
-  const onClickCategory =(index) => {
-     setActiveIndex(index)
-   }
-  return(
-   <div className="categories">
-    <ul>
-
-      {categories.map((el, i) => <li key={i} onClick={() => onClickCategory(i)} className={activeIndex === i ? 'active' : ''}>{el}</li>)}
-      
-    </ul>
-   </div>
-  )
+  return (
+    <div className="categories">
+      <ul>
+        {categories.map((categoryName, i) => (
+          <li
+            key={i}
+            onClick={() => onChangeCategory(i)}
+            className={value === i ? "active" : ''}
+          >
+            {categoryName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
-export default Categories
+export default Categories;
--- react-pizza/src/pages/Home.jsx
+++ react-pizza/src/pages/Home.jsx
@@ -7,21 +7,38 @@ import Sort from "../components/Sort";
 
 function Home() {
   const [items, setItems] = useState([]);
+  const [categoryId, setCategoryId] = useState(0);
+  const [sortType, setSortType] = useState({
+    name: "популярности",
+    sortProperty: "rating",
+  });
+
   useEffect(() => {
-    fetch("https:62f4c3c7535c0c50e761b9aa.mockapi.io/items")
+    
+    const sortBy = sortType.sortProperty.replace('-', '')
+    const order = sortType.sortProperty.includes('-') ? 'asc' : 'desc'
+    const category = categoryId > 0 ? `category=${categoryId}` : ''
+
+    fetch(
+      `https:62f4c3c7535c0c50e761b9aa.mockapi.io/items?${category}&sortBy=${sortBy}&order=${order}`,
+    )
       .then((res) => res.json())
       .then((json) => {
         setItems(json);
       });
-      window.scrollTo(0, 0)  // чтобы страница начиналась сверху, это для верстки
-  }, []);
+    window.scrollTo(0, 0); // чтобы страница начиналась сверху, это для верстки
+  }, [categoryId, sortType]);
+
 
   return (
     <div className="container">
       <div className="content__top">
-        <Categories />
+        <Categories
+          value={categoryId}
+          onChangeCategory={(i) => setCategoryId(i)}
+        />
 
-        <Sort />
+        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
