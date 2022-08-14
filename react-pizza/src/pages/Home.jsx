33d9162dfe3405a33a3a7c038ee02900cc0739bc
Murad <murad186992@mail.ru>
2022-08-14 20:53:44 +0300

    reduxSort

--- react-pizza/src/pages/Home.jsx
+++ react-pizza/src/pages/Home.jsx
@@ -11,22 +11,21 @@ import { setCategoryId } from "../redux/slices/filterSlice";
 function Home() {
   const dispatch = useDispatch();
   const categoryId = useSelector((state) => state.filter.categoryId);
+  const sortType = useSelector((state) => state.filter.sort.sortProperty);
+
 
   const { searchValue } = useContext(SearchContext);
   const [items, setItems] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [sortType, setSortType] = useState({
-    name: "популярности",
-    sortProperty: "rating",
-  });
+  
 
   const onChangeCategory = (id) => {
     dispatch(setCategoryId(id));
   };
 
   useEffect(() => {
-    const sortBy = sortType.sortProperty.replace("-", "");
-    const order = sortType.sortProperty.includes("-") ? "asc" : "desc";
+    const sortBy = sortType.replace("-", "");
+    const order = sortType.includes("-") ? "asc" : "desc";
     const category = categoryId > 0 ? `category=${categoryId}` : "";
     const search = searchValue ? `&search=${searchValue}` : "";
 
@@ -45,7 +44,7 @@ function Home() {
       <div className="content__top">
         <Categories value={categoryId} onChangeCategory={onChangeCategory} />
 
-        <Sort value={sortType} onChangeSort={(i) => setSortType(i)} />
+        <Sort />
       </div>
       <h2 className="content__title">Все пиццы</h2>
       <div className="content__items">
