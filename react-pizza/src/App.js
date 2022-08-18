d410cf2e318807ca0f338969241716756780bdd1
Murad <murad186992@mail.ru>
2022-08-18 19:03:55 +0300

    useLocation

--- react-pizza/src/App.js
+++ react-pizza/src/App.js
@@ -4,10 +4,10 @@ import Header from "./components/Header";
 import Home from "./pages/Home";
 import Cart from "./pages/Cart";
 import NotFound from "./pages/NotFound";
+import FullPizza from "./pages/FullPizza";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./scss/app.scss";
-
 function App() {
   return (
     <div className="wrapper">
@@ -17,6 +17,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
+          {/* <Route path="/pizza/:id" element={<FullPizza />} /> */}
           <Route path="*" element={<NotFound />} />
         </Routes>
       </div>
---
+++ react-pizza/src/pages/FullPizza.jsx
@@ -0,0 +1,17 @@
+// import React from 'react'
+// import { useParams } from 'react-router-dom'
+
+// function FullPizza() {
+//   const params = useParams()
+//   return (
+//     <div className='container' >
+//       <img src='' />
+//       <h2>{params.id}</h2>
+//       <p>fdjkhkdjhgfhgjkdfjgf</p>
+//       <h4>250 ₽</h4>
+//     </div>
+//   )
+// }
+
+// export default FullPizza
+
